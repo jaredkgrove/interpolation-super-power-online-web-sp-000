b7b3ed3f189c01cdf3a5c65628c04e7d32971b51
Jared Grove <jaredkgrove@gmail.com>
2019-03-02 20:10:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 # Write your #display_rainbow method here
-
+bob     = ["Bob", 46, "Juggler"]
+stefani = ["Stefani", 49, "Firefighter"]
 def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
 end
